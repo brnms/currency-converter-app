a8b267e81be294009364b6fe868ecf8959cba5e0
Bruno Santos <bruno.matasantos@gmail.com>
2020-11-04 18:01:23 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, {useEffect, } from 'react';
+import './App.css';
+import CurrencyRow from './CurrencyRow'
+
+const BASE_URL = 'https://api.exchangeratesapi.io/latest'
+
+function App() {
+
+  useEffect(() => {
+
+    fetch(BASE_URL)
+    .then(res => res.json())
+    .then(data => console.log(data))
+  }, [])
+
+  return (
+    <>
+    <h1>Convert Currency App</h1>
+    <CurrencyRow />
+      <div className="equals">=</div>
+    <CurrencyRow />
+
+    </>
+  );
+}
+
+export default App;
